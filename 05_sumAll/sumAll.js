ff4aea9b2612f944709a7da125d026af58d5a1bf
Zac <zacheryngky@gmail.com>
2022-08-28 01:31:47 +0800

    Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+//function uses sum of Arithmetic Sequence formula to find the sum of every number between and including 2 arguments
 
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    } else if ((num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    } else {
+        return (Math.abs(num1 - num2) + 1) * (num1 + num2) / 2;
+    }
 };
 
 // Do not edit below this line
