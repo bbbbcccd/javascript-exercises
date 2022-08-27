5b239cc512e5e82ec5b326ddbc569c891b439f10
Zac <zacheryngky@gmail.com>
2022-08-28 00:20:36 +0800

    Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let resultString = '';
+    for (; times > 0; times--) {
+        resultString = resultString.concat(str);
+    }
+    return resultString;
 };
 
 // Do not edit below this line
