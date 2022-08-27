3a4913c75f4033535585443c97367f54a0956c85
Zac <zacheryngky@gmail.com>
2022-08-28 01:12:50 +0800

    Complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+// For each rest parameter, check if it exists in given array
+// If it exists, remove element from array
 
+const removeFromArray = function(myArr, ...restParams) {
+    for (const restParam of restParams) {
+        for (let i = 0; i < myArr.length; i++) {
+            if (restParam === myArr[i]) {
+                myArr.splice(i, 1);
+                break;  
+            }
+        }
+    }
+    return myArr;
 };
 
 // Do not edit below this line
