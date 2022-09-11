264c7a7b456691deb74429053841667d49df0e13
Zac <zacheryngky@gmail.com>
2022-09-11 20:44:39 +0800

    Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^\w\s\']|_/g, "")
+            .replace(/\s/g, "")
+            .toLowerCase();
+    const length = str.length;
+    if (length % 2 === 0) {
+        const begin = str.slice(0, (length / 2));
+        const end = str.slice((length / 2)).split("").reverse().join("");
+        return begin === end;
+    } else {
+        const begin = str.slice(0, ((length - 1)/ 2));
+        const end = str.slice((length + 1) / 2).split("").reverse().join("");
+        return begin === end;
+    }
 };
 
 // Do not edit below this line
