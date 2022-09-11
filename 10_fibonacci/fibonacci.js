c2b6fd7ee566c6d10b8f15a53f779968bd6f8742
Zac <zacheryngky@gmail.com>
2022-09-11 21:08:09 +0800

    Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    function fib(a, b, i) {
+        if (i === 1) {
+            return a;
+        } else if (i === 2) {
+            return b;
+        } else {
+            return fib(b, a + b, i - 1);
+        }
+    }
+    if (isNaN(n) || n < 0) {
+        return "OOPS";
+    } else {
+        return fib(1, 1, +n);
+    }
 };
 
 // Do not edit below this line
