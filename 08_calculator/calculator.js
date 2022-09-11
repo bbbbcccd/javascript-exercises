bc78e2b310b67ab496391056c30a6e6bce7bbbe8
Zac <zacheryngky@gmail.com>
2022-09-11 20:19:40 +0800

    Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((previous, current) => {
+    return previous * current;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  if (b === 0) {
+    return 1;
+  } else {
+    return a * power(a, b -1);
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	function fact(acc, n) {
+    if (n === 0) {
+      return acc;
+    } else {
+      return fact(acc * n, n -1);
+    }
+  }
+  return fact(1,n);
 };
 
 // Do not edit below this line
