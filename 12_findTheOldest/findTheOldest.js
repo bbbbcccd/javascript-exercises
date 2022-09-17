f14fb257329ac92ebcdc3e3526708a7534f2f6e1
Zac <zacheryngky@gmail.com>
2022-09-17 23:43:42 +0800

    Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(person) {
+        if (person.yearOfDeath === undefined) {
+            return new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+    }
 
+    return people.reduce((previousPerson, currentPerson) => {
+        return getAge(previousPerson) > getAge(currentPerson) ? previousPerson : currentPerson;
+    });
 };
 
 // Do not edit below this line
